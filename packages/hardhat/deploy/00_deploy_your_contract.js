4ad09b1fedec656012a5e208edce924497573a5b
dbaonam99 <dbaonam99@gmail.com>
2022-06-05 22:20:01 +0700

    Update deploy script

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -4,26 +4,27 @@ module.exports = async () => {
   const Token = await hre.ethers.getContractFactory("Token");
   const token = await Token.deploy();
   await token.deployed();
-
   console.log("Token deployed to:", token.address);
-
-  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
-  const nftMarket = await NFTMarket.deploy();
-
-  await nftMarket.deployed();
-
-  console.log("NFTMarket deployed to:", nftMarket.address);
 
   const NFT = await hre.ethers.getContractFactory("NFT");
   const nft = await NFT.deploy();
-
   await nft.deployed();
   console.log("NFT deployed to:", nft.address);
 
   const Auction = await hre.ethers.getContractFactory("NFTAuction");
-  const auction = await Auction.deploy(token.address);
-
+  const auction = await Auction.deploy(
+    token.address,
+    "0x8C1bB3eb266b6416278a039de9b61b546093C437"
+  );
   await auction.deployed();
   console.log("Auction deployed to:", auction.address);
+
+  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const nftMarket = await NFTMarket.deploy(
+    token.address,
+    "0x8C1bB3eb266b6416278a039de9b61b546093C437"
+  );
+  await nftMarket.deployed();
+  console.log("NFTMarket deployed to:", nftMarket.address);
 };
 module.exports.tags = ["NFT"];
